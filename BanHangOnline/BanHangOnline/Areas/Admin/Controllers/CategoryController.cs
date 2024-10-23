2ed1b24b48ba0c9083547a00aaf0e15f546e88a9
unknown <ltthuynga03@gmail.com>
2024-10-23 15:13:38 +0700

    ghep bai lan 1

---
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BanHangOnline.Models;
+using BanHangOnline.Models.EF;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var items = db.categories;
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category model) 
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = BanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.categories.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = db.categories.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categories.Attach(model);
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = BanHangOnline.Models.Common.Filter.FilterChar(model.Title);
+                db.Entry(model).Property(x => x.Title).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.Alias).IsModified = true;
+                db.Entry(model).Property(x => x.SeoTitle).IsModified = true;
+                db.Entry(model).Property(x => x.SeoDescription).IsModified = true;
+                db.Entry(model).Property(x => x.SeoKeywords).IsModified = true;
+                db.Entry(model).Property(x => x.Position).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedDate).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedBy).IsModified = true;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.categories.Find(id);
+            if(item != null)
+            {
+                //var DeleteItem = db.categories.Attach(item);
+                db.categories.Remove(item);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }    
+            return Json(new { success = false });
+        }
+    }
+}
---
+++ BanHangOnline/BanHangOnline/Models/EF/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BanHangOnline.Models.EF
+{
+    [Table("tb_OrderDetail")]
+    public class OrderDetail
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Order orders {  get; set; }
+        public virtual Product products { get; set; }
+    }
+}
